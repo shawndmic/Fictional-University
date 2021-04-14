13f076e8adac16a05083a2f2cbfd72518cec74d5
Dean Cheukalam <dean.cheukalam@hakrinbank.com>
2021-04-14 13:11:14 -0300

    Dynamicly set menu for pages with sub pages

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -26,15 +26,32 @@ while(have_posts()){
   <?php
 } ?>
 
-<!--
+<?php
+  $test_array = get_pages(array
+    ('child_of' => get_the_ID()));
+
+if($the_parent or $test_array) { ?>
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($the_parent); ?>"><?php echo get_the_title($the_parent); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+      <?php
+
+      if($the_parent){
+        $findChilderenOf = $the_parent;
+      }
+      else{
+        $findChilderenOf = get_the_ID();
+      }
+
+        wp_list_pages(array
+          ('title_li' => NULL,
+            'child_of' => $findChilderenOf,
+            'sort_column' => 'menu_order'));
+      ?>
       </ul>
     </div>
--->
+
+<?php } ?>
 
     <div class="generic-content">
       <?php the_content(); ?>
